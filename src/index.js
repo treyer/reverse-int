dc8d891f7721920b86d676df22f33ede94b92ffb
treyer <nemereno_by@mail.ru>
2021-08-17 00:38:44 +0300

    feat: reverse function implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n = -n;
+    let numInString = String(n);
+    let res = '';
+    for (let i = 0; i < numInString.length; i++) {
+       res = numInString[i] + res;
+    }
+    return +res;
 }
